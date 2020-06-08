2a23516bafb9e258aa1496627dbb23411f46e609
AdbiyaSithwath <66269369+AdbiyaSithwath@users.noreply.github.com>
2020-06-08 21:02:45 +0530

    SwitchCase.java
    This file shows an example program of Switch Statement.
    
    Thank You!

--- SwitchCase.java
+++ SwitchCase.java
@@ -1,3 +1,19 @@
 import java.util.Scanner;
 public class SwitchCase{
-public static void main(String[] args){
+  public static void main(String[] args){
+    int select;
+    System.out.println("Your fav color is: 1.Black\t2.Blue\t3.Pink\t");//1
+    Scanner sc = new Scanner(System.in);
+    select = sc.nextInt();
+    switch(select){
+      case 1: System.out.println("Your fav color is Black");
+             break;
+      case 2: System.out.println("Your fav color is Blue");
+             break;
+      case 3: System.out.println("Your fav color is Pink");
+             break;
+      default: System.out.println("Invalid Choice");
+        //Your fav color is Black
+    }
+  }
+}
